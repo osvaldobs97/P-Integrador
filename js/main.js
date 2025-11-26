dc32475a171de17e2f33d62601ba1ae58b47bfad
Ivangcit <gunstriptor@gmail.com>
2025-11-25 19:55:07 -0600

    Se agrega el envio de correos, alertas, se agrega id al form

--- js/main.js
+++ js/main.js
@@ -9,6 +9,31 @@ const txtMensaje = document.getElementById("inputMensaje");
 const formularioContacto = document.getElementById("formularioContacto");
 
 
+//Correos
+//Iniciar EmailJS
+(function () {
+    emailjs.init("EXX6miCFreEdge_mH");  // <- reemplaza
+})();
+
+function enviarCorreo() {
+    const templateParams = {
+        from_name: document.getElementById("nombre").value,
+        from_email: document.getElementById("correo").value,
+        phone: document.getElementById("telefono").value,
+        message: document.getElementById("mensaje").value
+    };
+
+    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams) // <- reemplaza
+        .then(function (response) {
+            alert("Mensaje enviado correctamente ❤️");
+        })
+        .catch(function (error) {
+            console.error("Error:", error);
+            alert("Error al enviar el mensaje 😥");
+        });
+}
+//correos
+
 
 
 function validarNombre(nombre) {
@@ -28,7 +53,9 @@ function validarTelefono(telefono) {
     return formatoCorrecto && longitudCorrecta;
 }//validarTelefono
 
-
+function validarMensaje(mensaje){
+    return(txtMensaje.value.trim().length > 50);
+}//validarMensaje
 
 btnSubmit.addEventListener("click", function (event) {
     event.preventDefault();
@@ -36,22 +63,69 @@ btnSubmit.addEventListener("click", function (event) {
     let isValid = true;
 
     if (!validarNombre(txtNombre.value)) {
+
+        Swal.fire({
+            icon: "warning",
+            title: "Nombre incorrecto",
+            text: "El nombre esta mal escrito, verifique",
+        });
+
         console.log("El nombre esta mal escrito, verifique");
         isValid = false;
     }
 
     if (!validarEmail(txtCorreo.value)) {
+        Swal.fire({
+            icon: "warning",
+            title: "Correo incorrecto",
+            text: "El correo esta mal escrito, verifique",
+        });
+
         console.log("El correo esta mal escrito, verifique");
         isValid = false;
     }
 
     if (!validarTelefono(txtTelefono.value)) {
+        Swal.fire({
+            icon: "warning",
+            title: "Numero incorrecto",
+            text: "El numero esta mal escrito, verifique",
+        });
+
         console.log("El numero esta mal escrito, verifique");
         isValid = false;
     }
 
+    if(!validarMensaje(txtMensaje.value)){
+        Swal.fire({
+            icon: "warning",
+            title: "Mensaje corto",
+            text: "Escribe al menos 50 caracteres",
+        });
+        isValid=false;
+    }
+
+
     if (isValid) {
         console.log("Todo bien");
-    }
+
+        Swal.fire({
+  icon: "success",
+  title: "¡Mensaje enviado!",
+  text: "Nos pondremos en contacto contigo muy pronto.",
+});
+
+
+        emailjs.send("service_o9tr7a5", "template_zquzcob", {
+            name: txtNombre.value,
+            email: txtCorreo.value,
+            phone: txtTelefono.value,
+            message: txtMensaje.value
+
+        });
+    
+        formularioContacto.reset();
+
+    }//isValid
 
 });
