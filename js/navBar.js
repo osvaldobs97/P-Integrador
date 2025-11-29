d9f5f70cf84bb8f6a6b18b9e619177cdb6ef6fe4
OmarMarg <omarmtzarg@gmail.com>
2025-11-28 18:00:08 -0600

    Se vinculan las páginas a la barra de navegación y se integra la navegación entre páginas

--- js/navBar.js
+++ js/navBar.js
@@ -1,6 +1,6 @@
 const header = document.getElementById("navBar");
 header.className =
-  "d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom";
+    "d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom";
 header.innerHTML = `
     <div class="col-md-3 mb-2 mb-md-0">
         <a href="/" class="d-inline-flex align-items-center text-decoration-none">
@@ -10,8 +10,14 @@ header.innerHTML = `
     </div>
     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="index.html" class="nav-link px-2 link-secondary">Inicio</a></li>
+        <li><a href="productos.html" class="nav-link px-2 link-secondary">Productos</a></li>
+        <li><a href="formCrear.html" class="nav-link px-2 link-secondary">Crea tu producto</a></li>
         <li><a href="contacto.html" class="nav-link px-2 link-secondary">Contacto</a></li>
         <li><a href="acercaDe.html" class="nav-link px-2 link-secondary">Acerca de nosotros</a></li>
     </ul>
-    <div class="col-md-3 text-end"></div>
+    <div class="col-md-3 text-end">
+        <ul>
+
+        </ul>
+    </div>
 `;
