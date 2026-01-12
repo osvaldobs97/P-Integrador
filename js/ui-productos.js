df990430093eb7224671e2e0593c33f493045e1b
Rafa Cuevas <jaimerafaelgasparcuevas@gmail.com>
2026-01-12 17:29:11 -0600

    Se elimino el boton extra de carrito de compras

--- js/ui-productos.js
+++ js/ui-productos.js
@@ -21,7 +21,6 @@ function crearCards(productos) {
             <p class="card-text flex-grow-1">${producto.descripcion}</p>
             <p class="fw-bold mb-2">Desde $${producto.precio} MXN</p>
             <button class="btn btn-primary mt-auto">Personalizar</button>
-            <button class="btn btn-primary mt-2">Aregar al Carrito</button>
             <button class="btn btn-primary mt-2 btn-agregar" data-id="${producto.id}">Agregar al carrito</button>
           </div>
         </div>
