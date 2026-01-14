54006bbfabb461282cfb59e6db00e02139508071
Ivangcit <gunstriptor@gmail.com>
2026-01-13 20:28:28 -0600

    Se agrega funcion de redireccion del boton de Personalizar en las cards de productos

--- js/ui-productos.js
+++ js/ui-productos.js
@@ -39,7 +39,7 @@ function crearCards(productos) {
             <h5 class="card-title">${producto.nombre}</h5>
             <p class="card-text flex-grow-1">${producto.descripcion}</p>
             <p class="fw-bold mb-2">Desde $${producto.precio} MXN</p>
-            <button class="btn btn-primary mt-auto">Personalizar</button>
+            <button class="btn btn-primary mt-auto btn-personalizar" data-id="${producto.id}">Personalizar</button>
             <button class="btn btn-primary mt-2 btn-agregar" data-id="${producto.id}">Agregar al carrito</button>
           </div>
         </div>
@@ -47,7 +47,41 @@ function crearCards(productos) {
     `;
   });
 
+
   container.insertAdjacentHTML("beforeend", html);
   activarBotonesCarrito(productos);
+  activarBotonesPersonalizar(productos);
 }
 
+function activarBotonesPersonalizar(productos) {
+  const botones = document.querySelectorAll(".btn-personalizar");
+
+  // Mapeo de IDs del JSON a IDs del HTML en personalizar.html
+  const jsonIdToHtmlId = {
+    1: "item-1", // Playera
+    2: "item-2", // Taza blanca
+    3: "item-3", // Termo
+    4: "item-4", // Taza mágica
+    5: "item-5", // Agenda
+    6: "item-6",// Sudadera
+    7: "item-7", // Gorra
+    8: "item-8", // Pluma
+    9: "item-9", // Llavero
+    10: "item-10" // Bolsa
+  };
+
+  botones.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const id = Number(btn.dataset.id);
+      const htmlId = jsonIdToHtmlId[id];
+
+      if (htmlId) {
+        window.location.href = `personalizar.html?product=${htmlId}`;
+      } else {
+        console.error("ID de producto no mapeado:", id);
+        // Redirección por defecto si falla el mapeo
+        window.location.href = `personalizar.html`;
+      }
+    });
+  });
+}
